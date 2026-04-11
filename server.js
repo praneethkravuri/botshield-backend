846f832f1d9b7de5c8bf48bacbde3a27915df641
praneethkravuri <praneethkravuri@gmail.com>
2026-04-10 23:40:26 -0500

    Update server.js to include install endpoint

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
-
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Backend is running!');
+    res.send('Backend is running!');
 });
 
 // 👇 PASTE IT HERE
 app.get('/install', (req, res) => {
-  res.send('Install route coming soon 🚀');
+    const shop = req.query.shop;
+
+    res.send(`Installing app for ${shop}... 🚀`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+    console.log(`Server listening on port ${PORT}`);
+})
