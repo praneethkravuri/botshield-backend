90691fd1f1013c0beb95558e557c34e5f9a6074a
praneethkravuri <praneethkravuri@gmail.com>
2026-04-10 23:23:51 -0500

    Update server.js to add install route

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Backend is running!');
+});
+
+// 👇 PASTE IT HERE
+app.get('/install', (req, res) => {
+  res.send('Install route coming soon 🚀');
 });
 
 app.listen(PORT, () => {
