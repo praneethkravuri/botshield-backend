7c88ee312c29dc2f78816121fd7d1886cf7905d1
praneethkravuri <praneethkravuri@gmail.com>
2026-08-10 17:56:33 -0500

    Expose authenticated financial impact endpoint

---
+++ app/routes/api.financial-impact.jsx
@@ -0,0 +1,9 @@
+import db from "../db.server";
+import { getEstimatedValueProtected } from "../lib/financial-impact.server";
+import { authenticate } from "../shopify.server";
+
+export async function loader({ request }) {
+  const { session } = await authenticate.admin(request);
+  const impact = await getEstimatedValueProtected(db, session.shop);
+  return Response.json({ impact });
+}
